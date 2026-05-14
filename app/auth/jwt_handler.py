a8372712af6690e665a76e0a0f1cbec527541fe5
Alexandru Crainiciuc <alexandrru.crainiciuc91@gmail.com>
2026-05-14 09:04:02 +0300

    Move secrets to env

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,35 +1,81 @@
-from jose import jwt
-from datetime import datetime, timedelta, timezone
+import os
 
-SECRET_KEY = "SUPER_SECRET_KEY"
+from datetime import (
+    datetime,
+    timedelta,
+    timezone
+)
+
+from jose import jwt
+
+from dotenv import load_dotenv
+
+# ============================================
+# LOAD ENV
+# ============================================
+
+load_dotenv()
+
+# ============================================
+# JWT CONFIG
+# ============================================
+
+SECRET_KEY = os.getenv(
+    "JWT_SECRET"
+)
 
 ALGORITHM = "HS256"
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 100000
 
+# ============================================
+# CREATE ACCESS TOKEN
+# ============================================
 
-def create_access_token(data: dict):
+def create_access_token(
+        data: dict
+):
 
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    expire = (
+        datetime.now(
+            timezone.utc
+        ) + timedelta(
+            minutes=
+            ACCESS_TOKEN_EXPIRE_MINUTES
+        )
     )
 
-    to_encode.update({"exp": expire})
+    to_encode.update({
+        "exp": expire
+    })
 
     encoded_jwt = jwt.encode(
+
         to_encode,
+
         SECRET_KEY,
+
         algorithm=ALGORITHM
     )
 
     return encoded_jwt
-def verify_token(token: str):
+
+# ============================================
+# VERIFY TOKEN
+# ============================================
+
+def verify_token(
+        token: str
+):
 
     payload = jwt.decode(
+
         token,
+
         SECRET_KEY,
+
         algorithms=[ALGORITHM]
     )
 
