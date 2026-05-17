fedee6879f183751f80f69f7a130f7a8ab55ae71
Alexandru Crainiciuc <alexandrru.crainiciuc91@gmail.com>
2026-05-17 21:10:54 +0300

    test without db init

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
 app.include_router(driver_profile_router)
