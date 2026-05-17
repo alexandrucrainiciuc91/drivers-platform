b21bc18a6790b0e8011539a71308ce3b20ab4a87
Alexandru Crainiciuc <alexandrru.crainiciuc91@gmail.com>
2026-05-17 17:01:42 +0300

    fix cors

--- main.py
+++ main.py
@@ -54,15 +54,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.1.137:3000",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
