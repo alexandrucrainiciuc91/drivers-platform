9817221c0bf3e040a2ddd8f82f71061fb4ca8087
Alexandru Crainiciuc <alexandrru.crainiciuc91@gmail.com>
2026-05-17 20:24:22 +0300

    fix railway postgres url

--- app/database/database.py
+++ app/database/database.py
@@ -4,9 +4,14 @@ from sqlalchemy.orm import declarative_base
 
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
 engine = create_engine(DATABASE_URL)
 
