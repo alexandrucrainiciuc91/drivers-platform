82492df9a16413de0ff5ba294e933cabae306915
Alexandru Crainiciuc <alexandrru.crainiciuc91@gmail.com>
2026-05-25 22:52:05 +0300

    fix auth localstorage flow

--- drivers-platform-frontend-clean/app/login/page.tsx
+++ drivers-platform-frontend-clean/app/login/page.tsx
@@ -150,10 +150,7 @@ export default function LoginPage() {
         "token",
         data.access_token
       );
-localStorage.setItem(
-    "user_id",
-    data.user_id
-);
+
       // SAVE USER TYPE
 
       localStorage.setItem(
